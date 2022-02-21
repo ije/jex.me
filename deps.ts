03bdda452b8e2fc3dc7576345a56e732698305ac
X <i@jex.me>
2022-02-22 05:05:12 +0800

    Use aleph.js

--- deps.ts
+++
@@ -1,2 +0,0 @@
-export { serve } from "https://deno.land/std@0.123.0/http/server.ts"
-export { relative as getRelativePath, extname, join } from "https://deno.land/std@0.123.0/path/mod.ts"
---
+++ server.ts
@@ -0,0 +1,3 @@
+import { serve } from "https://deno.land/x/aleph_canary@0.0.4/server/mod.ts";
+
+serve();
