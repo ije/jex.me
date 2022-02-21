5e9ac25386671a113f4b1a271cc7ce3d562e8e23
X <i@jex.me>
2022-02-22 05:16:54 +0800

    Update `/fs/` route

--- server.ts
+++ server.ts
@@ -1,3 +1,16 @@
 import { serve } from "https://deno.land/x/aleph_canary@0.0.4/server/mod.ts";
 
-serve();
+serve({
+  fetch: async (req) => {
+    const { pathname } = new URL(req.url);
+    if (pathname.startsWith("/fs/")) {
+      const list: Deno.DirEntry[] = [];
+      for await (const e of Deno.readDir(pathname.slice(4) || ".")) {
+        list.push(e);
+      }
+      return new Response(JSON.stringify(list), {
+        headers: { "content-type": "application/json" },
+      });
+    }
+  },
+});
