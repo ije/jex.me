13be81304ec4deb63f7ee049f4d4f8c6186ef05d
X <i@jex.me>
2022-02-22 05:23:55 +0800

    test

--- server.ts
+++ server.ts
@@ -3,6 +3,12 @@ import { serve } from "https://deno.land/x/aleph_canary@0.0.4/server/mod.ts";
 serve({
   fetch: async (req) => {
     const { pathname } = new URL(req.url);
+    if (pathname === "/fs/src/world.glsl") {
+      const stat = await Deno.lstat("./src/world.glsl");
+      return new Response(JSON.stringify(stat), {
+        headers: { "content-type": "application/json" },
+      });
+    }
     if (pathname.startsWith("/fs/")) {
       const list: Deno.DirEntry[] = [];
       for await (const e of Deno.readDir(pathname.slice(4) || ".")) {
